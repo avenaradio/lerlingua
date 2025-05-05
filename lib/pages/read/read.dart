62ec828a801840462503a4c453be08669d45f30d
avenaradio <avenaradio@mailbox.org>
2025-05-05 19:04:55 +0200

    load and view epub file closes #3
    Took 1 hour 15 minutes

--- lib/pages/read/read.dart
+++ lib/pages/read/read.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:lerlingua/pages/read/webview.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:lerlingua/pages/read/epub_view.dart';
+
+import '../settings/file_handler.dart';
 
 class Read extends StatefulWidget {
   const Read({super.key});
@@ -10,11 +13,35 @@ class Read extends StatefulWidget {
 }
 
 class _ReadState extends State<Read> {
+  File? file;
 
+  loadFile() async {
+    file = await FileHandler.loadEpubFile();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    loadFile();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WebViewExample(),
+      body: Column(
+          children: [
+            Expanded(
+              flex: 50,
+              child: file != null ? EpubView(title: 'dummy title', file: file!) : const Placeholder(),
+            ),
+            Expanded(
+              flex: 20, // TODO make this dynamic
+              child: Text('translation widget'),
+            ),
+
+          ]
+      ),
+
     );
   }
 }
