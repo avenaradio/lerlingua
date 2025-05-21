01baa9eefa7c0ecda8878b9d8836e040445c2f9d
avenaradio <avenaradio@mailbox.org>
2025-05-21 14:36:16 +0200

    sync.dart (github integration) tested
    Took 2 hours 24 minutes

--- lib/resources/sync.dart
+++ lib/resources/sync.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+enum FileType { test, cards, settings }
+
 class Sync {
   String syncLog = 'Sync Log:\n';
   String token = '';
@@ -26,13 +28,40 @@ class Sync {
     return this;
   }
 
-  String get url => 'https://api.github.com/repos/$repoOwner/$repoName/contents/$filePath';
+  String url({required FileType fileType}) {
+    switch (fileType) {
+      case FileType.test:
+        return 'https://api.github.com/repos/$repoOwner/$repoName/contents/test.json';
+      case FileType.cards:
+        return 'https://api.github.com/repos/$repoOwner/$repoName/contents/cards.json';
+      case FileType.settings:
+        return 'https://api.github.com/repos/$repoOwner/$repoName/contents/settings.json';
+    }
+  }
+  void clearLog() => syncLog = 'Sync Log:\n';
 
-  /// Function to upload map to GitHub
-  Future<bool> uploadJsonToGitHub(String jsonString) async {
+  /// Converts an upload response to a string
+  String stringFromUploadResponse({required int uploadResponse}) {
+    switch (uploadResponse) {
+      case -2:
+        return 'Repo not found but token accepted';
+      case -1:
+        return 'Bad credentials';
+      case 0:
+        return 'Unknown error';
+      default:
+        return 'Success';
+    }
+  }
+
+  /// Uploads a json string to GitHub <br>
+  /// Returns 1 if file created, 2 if file updated, -1 if bad credentials, -2 if repo not found but token accepted, 0 if error
+  /// - Tested
+  Future<int> uploadJsonToGitHub({required String jsonString, required FileType fileType}) async {
+    int result = 0;
     // First, check if the file exists to get its sha
     final responseGet = await http.get(
-      Uri.parse(url),
+      Uri.parse(url(fileType: fileType)),
       headers: {
         'Authorization': 'token $token',
         'Accept': 'application/vnd.github.v3+json',
@@ -43,6 +72,19 @@ class Sync {
       // File exists, get the sha
       final Map<String, dynamic> fileData = jsonDecode(responseGet.body);
       sha = fileData['sha'];
+      result = 2;
+      syncLog += ('File already exists, will be updated...\n');
+    } else if (responseGet.statusCode == 404) {
+      // File does not exist, create it
+      result = 1;
+      syncLog += ('File does not exist, will be created...\n');
+    } else if (responseGet.statusCode == 401) {
+      // Bad credentials
+      syncLog += ('Bad credentials: ${responseGet.body}\n\n');
+      return -1;
+    } else {
+      syncLog += ('Failed to check if file exists: ${responseGet.body}\n\n');
+      return 0;
     }
     // Prepare the request body
     final Map<String, dynamic> body = {
@@ -55,7 +97,7 @@ class Sync {
     }
     // Make the PUT request to upload the file
     final responsePut = await http.put(
-      Uri.parse(url),
+      Uri.parse(url(fileType: fileType)),
       headers: {
         'Authorization': 'token $token',
         'Accept': 'application/vnd.github.v3+json',
@@ -63,18 +105,28 @@ class Sync {
       body: jsonEncode(body),
     );
     if (responsePut.statusCode == 201 || responsePut.statusCode == 200) {
-      syncLog += ('File uploaded successfully!');
-      return true;
+      syncLog += ('File uploaded successfully!\n');
+      return result;
+    } else if (responsePut.statusCode == 404) {
+      if (result == 1) {
+        // Repo not found
+        syncLog += ('Repo not found.\n');
+        syncLog += ('Failed to upload file: ${responsePut.body}\n\n');
+        return -2;
+      }
+      return 0;
     } else {
       syncLog += ('Failed to upload file: ${responsePut.body}\n\n');
-      return false;
+      return 0;
     }
   }
 
-  /// Function to download JSON file from GitHub
-  Future<String?> downloadJsonFromGithub() async {
+  /// Downloads JSON file from GitHub <br>
+  /// Returns null if error occurs, '&#91;&#93;' if file not found
+  /// - Tested
+  Future<String?> downloadJsonFromGithub({required FileType fileType}) async {
     final response = await http.get(
-      Uri.parse(url),
+      Uri.parse(url(fileType: fileType)),
       headers: {
         'Authorization': 'token $token',
         'Accept': 'application/vnd.github.v3+json',
@@ -83,15 +135,17 @@ class Sync {
     if (response.statusCode == 200) {
       final Map<String, dynamic> jsonResponse = jsonDecode(response.body);
       String content = jsonResponse['content'];
-      /*
-      // Cut off if length is not a multiple of 4
-      int contentModulo = content.length % 4;
-      if (contentModulo != 0) {
-        content = content.substring(0, content.length - contentModulo);
-      }*/
-      final String decodedContent = utf8.decode(base64Decode(content));
+      content = content.replaceAll(RegExp(r'[^A-Za-z0-9+/=]'), ''); // Remove invalid characters
+      String decodedContent = '';
+      try {
+        decodedContent = utf8.decode(base64Decode(content));
+      } catch (e) {
+        syncLog += ('Failed to decode file content: $e\n\n');
+        return null;
+      }
       syncLog += ('File downloaded successfully!\n');
-      syncLog += ('File content: $decodedContent\n\n');
+      syncLog += ('Raw file content: $content\n\n');
+      syncLog += ('Decoded file content: $decodedContent\n\n');
       return decodedContent;
     } else if (response.statusCode == 404) {
       syncLog += ('File not found: ${response.body}\n\n');
@@ -103,13 +157,14 @@ class Sync {
     }
   }
 
-  /// Deletes a file from GitHub repository via GitHub API.
+  /// Deletes a file from GitHub repository via GitHub API. <br>
   /// Returns 1 if the file was deleted successfully, 2 if the file was not found, and 0 otherwise.
-  Future<int> deleteFileOnGithub() async {
+  /// - Tested
+  Future<int> deleteFileOnGithub({required FileType fileType}) async {
     try {
       // Step 1: Get the file SHA
       final getResponse = await http.get(
-        Uri.parse(url),
+        Uri.parse(url(fileType: fileType)),
         headers: {
           'Authorization': 'token $token',
           'Accept': 'application/vnd.github.v3+json',
@@ -118,10 +173,9 @@ class Sync {
       if (getResponse.statusCode == 200) {
         final fileData = jsonDecode(getResponse.body);
         final String sha = fileData['sha'];
-
         // Step 2: Send DELETE request to delete the file with SHA
         final deleteResponse = await http.delete(
-          Uri.parse(url),
+          Uri.parse(url(fileType: fileType)),
           headers: {
             'Authorization': 'token $token',
             'Accept': 'application/vnd.github.v3+json',
@@ -141,7 +195,7 @@ class Sync {
         }
       } else if (getResponse.statusCode == 404) {
         syncLog += 'File not found: ${getResponse.body}\n';
-        return 1;
+        return 2;
       } else {
         syncLog += 'Failed to get file info: ${getResponse.statusCode} ${getResponse.body}\n';
         return 0;
--- test/resources/sync_test.dart
+++ test/resources/sync_test.dart
@@ -5,21 +5,155 @@ import 'package:lerlingua/resources/sync.dart';
 
 void main() {
   group('deleteFileOnGithub Tests', () {
-    test('token variable from enviroment', () {
+    test('token variable from enviroment should not be empty', () {
       expect(syncTestToken, isNot(''));
     });
-    test('credentials', () {
+    test('credentials should be set', () {
       Sync syncSingleton = Sync().credentials(token: 'test_token', repoOwner: 'test_owner', repoName: 'test_repo');
       expect(syncSingleton.token, 'test_token');
       expect(syncSingleton.repoOwner, 'test_owner');
       expect(syncSingleton.repoName, 'test_repo');
     });
-    test('deleteFileOnGithub', () async {
-      int result = await Sync().credentials(token: syncTestToken, repoOwner: 'avenaradio', repoName: 'lerlingua_sync_test').deleteFileOnGithub();
+    test('deleteFileOnGithub should return 1 if file exists', () async {
+      Sync().clearLog();
+      Sync().credentials(token: syncTestToken, repoOwner: 'avenaradio', repoName: 'lerlingua_sync_test');
+      await Sync().uploadJsonToGitHub(jsonString: '[{"test":"test"}]', fileType: FileType.test);
+      int result = await Sync().deleteFileOnGithub(fileType: FileType.test);
+      // Assert
       if (kDebugMode) {
+        // print test name
+        print('\ndeleteFileOnGithub should return 1 if file exists');
         print(Sync().syncLog);
       }
-      expect(result, greaterThan(0));
+      expect(result, 1);
+    });
+    test('deleteFileOnGithub should return 2 if file does not exist', () async {
+      Sync().clearLog();
+      Sync().credentials(token: syncTestToken, repoOwner: 'avenaradio', repoName: 'lerlingua_sync_test');
+      await Sync().deleteFileOnGithub(fileType: FileType.test);
+      // Repo should be empty at this point
+      int result = await Sync().deleteFileOnGithub(fileType: FileType.test);
+      // Assert
+      if (kDebugMode) {
+        // print test name
+        print('\ndeleteFileOnGithub should return 2 if file does not exist');
+        print(Sync().syncLog);
+      }
+      expect(result, 2);
+    });
+    test('deleteFileOnGithub wrong token should return 0', () async {
+      Sync().clearLog();
+      int result = await Sync().credentials(token: 'wrong_token', repoOwner: 'avenaradio', repoName: 'lerlingua_sync_test').deleteFileOnGithub(fileType: FileType.test);
+      // Assert
+      if (kDebugMode) {
+        // print test name
+        print('\ndeleteFileOnGithub wrong token should return 0');
+        print(Sync().syncLog);
+      }
+      expect(result, 0);
+    });
+    test('deleteFileOnGithub non-existant repo should return 2', () async {
+      Sync().clearLog();
+      int result = await Sync().credentials(token: syncTestToken, repoOwner: 'avenaradio', repoName: 'lerlingua_sync_test_non_existant_repo').deleteFileOnGithub(fileType: FileType.test);
+      // Assert
+      if (kDebugMode) {
+        // print test name
+        print('\ndeleteFileOnGithub non-existant repo should return 2');
+        print(Sync().syncLog);
+      }
+      expect(result, 2);
+    });
+  });
+  group('downloadJsonFromGithub Tests', () {
+    test('downloadJsonFromGithub if file does not exist should return "[]"', () async {
+      Sync().clearLog();
+      await Sync().credentials(token: syncTestToken, repoOwner: 'avenaradio', repoName: 'lerlingua_sync_test').deleteFileOnGithub(fileType: FileType.test);
+      String? jsonString = await Sync().downloadJsonFromGithub(fileType: FileType.test);
+      // Assert
+      if (kDebugMode) {
+        // print test name
+        print('\ndownloadJsonFromGithub if file does not exist should return "[]"');
+        print(Sync().syncLog);
+      }
+      expect(jsonString, '[]');
+    });
+    test('downloadJsonFromGithub if file exists should return file content', () async {
+      Sync().clearLog();
+      await Sync().credentials(token: syncTestToken, repoOwner: 'avenaradio', repoName: 'lerlingua_sync_test').deleteFileOnGithub(fileType: FileType.test);
+      await Sync().uploadJsonToGitHub(jsonString: '[{"test":"test"}]', fileType: FileType.test);
+      String? jsonString = await Sync().downloadJsonFromGithub(fileType: FileType.test);
+      // Assert
+      if (kDebugMode) {
+        // print test name
+        print('\ndownloadJsonFromGithub if file exists should return file content');
+        print(Sync().syncLog);
+      }
+      expect(jsonString, '[{"test":"test"}]');
+    });
+    test('downloadJsonFromGithub if error should return null', () async {
+      Sync().clearLog();
+      await Sync().credentials(token: 'wrong_token', repoOwner: 'avenaradio', repoName: 'lerlingua_sync_test').deleteFileOnGithub(fileType: FileType.test);
+      String? jsonString = await Sync().downloadJsonFromGithub(fileType: FileType.test);
+      // Assert
+      if (kDebugMode) {
+        // print test name
+        print('\ndownloadJsonFromGithub if error should return null');
+        print(Sync().syncLog);
+      }
+      expect(jsonString, null);
+    });
+  });
+  group('uploadJsonToGitHub Tests', () {
+    test('uploadJsonToGitHub if wrong token should return -1', () async {
+      Sync().clearLog();
+      await Sync().credentials(token: 'wrong_token', repoOwner: 'avenaradio', repoName: 'lerlingua_sync_test').deleteFileOnGithub(fileType: FileType.test);
+      int result = await Sync().uploadJsonToGitHub(jsonString: '[{"test":"test"}]', fileType: FileType.test);
+      // Assert
+      if (kDebugMode) {
+        // print test name
+        print('\nuploadJsonToGitHub if wrong token should return -1');
+        print(Sync().syncLog);
+      }
+      expect(result, -1);
+    });
+    test('uploadJsonToGitHub if correct token and wrong repo should return -2', () async {
+      Sync().clearLog();
+      await Sync().credentials(token: syncTestToken, repoOwner: 'non_existant', repoName: 'non_existant').deleteFileOnGithub(fileType: FileType.test);
+      int result = await Sync().uploadJsonToGitHub(jsonString: '[{"test":"test"}]', fileType: FileType.test);
+      // Assert
+      if (kDebugMode) {
+        // print test name
+        print('\nuploadJsonToGitHub if correct token and wrong repo should return -2');
+        print(Sync().syncLog);
+      }
+      expect(result, -2);
+    });
+    test('uploadJsonToGitHub if created should return 1', () async {
+      Sync().clearLog();
+      await Sync().credentials(token: syncTestToken, repoOwner: 'avenaradio', repoName: 'lerlingua_sync_test').deleteFileOnGithub(fileType: FileType.test);
+      int result = await Sync().uploadJsonToGitHub(jsonString: '[{"test":"test"}]', fileType: FileType.test);
+      // Assert
+      if (kDebugMode) {
+        // print test name
+        print('\nuploadJsonToGitHub if created should return 1');
+        print(Sync().syncLog);
+      }
+      expect(result, 1);
+      expect(await Sync().downloadJsonFromGithub(fileType: FileType.test), '[{"test":"test"}]');
+    });
+    test('uploadJsonToGitHub if updated should return 2', () async {
+      Sync().clearLog();
+      await Sync().credentials(token: syncTestToken, repoOwner: 'avenaradio', repoName: 'lerlingua_sync_test').deleteFileOnGithub(fileType: FileType.test);
+      await Sync().uploadJsonToGitHub(jsonString: '[{"test":"test1"}]', fileType: FileType.test);
+      int result = await Sync().uploadJsonToGitHub(jsonString: '[{"test":"test2"}]', fileType: FileType.test);
+      // Assert
+      if (kDebugMode) {
+        // print test name
+        print('\nuploadJsonToGitHub if updated should return 2');
+        print(Sync().syncLog);
+      }
+      expect(result, 2);
+      expect(await Sync().downloadJsonFromGithub(fileType: FileType.test), '[{"test":"test2"}]');
     });
   });
 }
