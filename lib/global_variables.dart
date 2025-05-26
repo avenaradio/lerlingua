7cb56818bb83d6c2921bf834bc9ffa6dd0747663
avenaradio <avenaradio@mailbox.org>
2025-05-26 12:15:03 +0200

    added feedback button
    Took 56 minutes

--- lib/global_variables.dart
+++ lib/global_variables.dart
@@ -1,3 +1,4 @@
 // flutter test --dart-define=IS_TEST=true
 const bool isTesting = bool.fromEnvironment('IS_TEST', defaultValue: false);
 const String syncTestToken = String.fromEnvironment('SYNC_TEST_TOKEN');
+const String feedbackToken = String.fromEnvironment('FEEDBACK_TOKEN');
