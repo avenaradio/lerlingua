8aecba4d71ed8e0aacc3c41b8e039a43ef81ceb2
avenaradio <avenaradio@mailbox.org>
2025-06-05 12:10:40 +0200

    snapshot
    Took 22 minutes

--- lib/general/string_extension.dart
+++ lib/general/string_extension.dart
@@ -5,4 +5,9 @@ extension StringExtension on String {
     }
     return this[0].toUpperCase() + substring(1);
   }
+
+  String trimNonAlphanumeric(String input) {
+    // Use a regular expression to match non-alphanumeric characters
+    return input.replaceAll(RegExp(r'^[^a-zA-Z0-9]+|[^a-zA-Z0-9]+$'), '');
+  }
 }
